f705357d7b17e7234036ce5ede21995bc5024a50
Miguel Angel Avalos Fernandez <miguelavalos@gmail.com>
2026-05-24 22:37:05 +0200

    Extract Moments remote date helpers

---
+++ apps/ios/MomentsAV/MomentsData/Sync/MomentsProjectRemoteClient+Dates.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension MomentsProjectRemoteClient {
+    func milliseconds(from date: Date) -> Double {
+        date.timeIntervalSince1970 * 1000
+    }
+
+    func expirationMilliseconds(from date: Date = Date()) -> Double {
+        date.addingTimeInterval(30 * 24 * 60 * 60).timeIntervalSince1970 * 1000
+    }
+}
--- apps/ios/MomentsAV/MomentsData/Sync/MomentsProjectRemoteClient.swift
+++ apps/ios/MomentsAV/MomentsData/Sync/MomentsProjectRemoteClient.swift
@@ -84,13 +84,3 @@ struct MomentsProjectRemoteClient {
         return client
     }
 }
-
-extension MomentsProjectRemoteClient {
-    func milliseconds(from date: Date) -> Double {
-        date.timeIntervalSince1970 * 1000
-    }
-
-    func expirationMilliseconds(from date: Date = Date()) -> Double {
-        date.addingTimeInterval(30 * 24 * 60 * 60).timeIntervalSince1970 * 1000
-    }
-}
